3764e951425c594afda430d1792c2137ac784641
sige <sige@sige.hu>
2019-02-11 14:24:40 +0100

    validate and handle intentions

--- Health.cs
+++ Health.cs
@@ -27,6 +27,12 @@ namespace ConfigCat.Function
             string json = await req.ReadAsStringAsync();
             var skillRequest = JsonConvert.DeserializeObject<SkillRequest>(json);
 
+            var isValid = await ValidateRequestAsync(req, skillRequest);
+            if (!isValid)
+            {
+                return new BadRequestResult();
+            }
+
             var requestType = skillRequest.GetRequestType();
 
             SkillResponse response = null;
@@ -36,7 +42,6 @@ namespace ConfigCat.Function
                 response = ResponseBuilder.Tell("Welcome to ConfigCat health checker!");
                 response.Response.ShouldEndSession = false;
             }
-
             else if (requestType == typeof(IntentRequest))
             {
                 var intentRequest = skillRequest.Request as IntentRequest;
@@ -47,13 +52,76 @@ namespace ConfigCat.Function
                     if (getResponse.IsSuccessStatusCode)
                     {
                         response = ResponseBuilder.Tell("All systems are green!");
-                    } else {
+                    }
+                    else
+                    {
                         response = ResponseBuilder.Tell("There is something wrong with the website.");
                     }
                 }
+                else if (intentRequest.Intent.Name == "AMAZON.HelpIntent")
+                {
+                    response = ResponseBuilder.Tell("You can ask config cat how he's feeling.");
+                    response.Response.ShouldEndSession = false;
+                }
+                else if (intentRequest.Intent.Name == "AMAZON.CancelIntent" || intentRequest.Intent.Name == "AMAZON.StopIntent")
+                {
+                    response = ResponseBuilder.Tell("Goodbye!");
+                    response.Response.ShouldEndSession = true;
+                }
+            }
+            else if (requestType == typeof(SessionEndedRequest))
+            {
+                response = ResponseBuilder.Empty();
+                response.Response.ShouldEndSession = true;
             }
 
             return new OkObjectResult(response);
+        }
+
+        private static async Task<bool> ValidateRequestAsync(HttpRequest request, SkillRequest skillRequest)
+        {
+            request.Headers.TryGetValue("SignatureCertChainUrl", out var signatureChainUrl);
+            if (string.IsNullOrWhiteSpace(signatureChainUrl))
+            {
+                return false;
+            }
+
+            Uri certUrl;
+            try
+            {
+                certUrl = new Uri(signatureChainUrl);
+            }
+            catch
+            {
+                return false;
+            }
+
+            request.Headers.TryGetValue("Signature", out var signature);
+            if (string.IsNullOrWhiteSpace(signature))
+            {
+                return false;
+            }
+
+            request.Body.Position = 0;
+            var body = await request.ReadAsStringAsync();
+            request.Body.Position = 0;
+
+            if (string.IsNullOrWhiteSpace(body))
+            {
+                return false;
+            }
+
+            bool isTimestampValid = RequestVerification.RequestTimestampWithinTolerance(skillRequest);
+            bool valid = await RequestVerification.Verify(signature, certUrl, body);
+
+            if (!valid || !isTimestampValid)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
